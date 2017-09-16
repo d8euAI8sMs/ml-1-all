d1e44bbeb93e0bd16f7e88b70da1bee7b3da2d89
kalaider <a.kalaider@yandex.ru>
2017-09-16 18:54:10 +0300

    Network prediction function implemented

--- ANNLib/ANNImpl.cpp
+++ ANNLib/ANNImpl.cpp
@@ -44,3 +44,24 @@ std::string ANN::BackPropagationNetwork::GetType()
 {
     return "Back Propagation Network by Vasilevsky Alexander";
 }
+
+std::vector<float> ANN::BackPropagationNetwork::Predict(std::vector<float> & input)
+{
+    std::vector < float > in = input;
+    std::vector < float > out;
+    for (size_t i = 0; i < weights.size(); ++i) // layers
+    {
+        out.resize(weights[i].size());
+        for (size_t j = 0; j < weights[i].size(); ++j) // neurons in i-th layer
+        {
+            float neuron_input = 0;
+            for (size_t k = 0; k < in.size(); ++k) // (i-1)-th layer output
+            {
+                neuron_input += in[k] * weights[i][j][k];
+            }
+            out[j] = Activation(neuron_input);
+        }
+        in.swap(out);
+    }
+    return in;
+}
