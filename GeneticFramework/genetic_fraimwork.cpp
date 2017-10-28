af3e3c10b3ab406d8454a78a4d171ede157e98bd
kalaider <a.kalaider@yandex.ru>
2017-10-28 16:45:34 +0300

    Basic tic-tac train and play mechanics implemented

--- GeneticFramework/genetic_fraimwork.cpp
+++ GeneticFramework/genetic_fraimwork.cpp
@@ -121,6 +121,99 @@ void xor_demo()
     } while (err > eps * eps);
 }
 
+void ttt_demo()
+{
+    size_t initial_size;
+    std::cout << "[input] Initial population size: [uint] ";
+    std::cin >> initial_size;
+    size_t max_epoch;
+    std::cout << "[input] Max epoch: [uint] ";
+    std::cin >> max_epoch;
+    size_t board_size;
+    std::cout << "[input] Board size: [uint] ";
+    std::cin >> board_size;
+
+    GeneticAlgorithm a;
+    a.epoch = std::make_shared < Epoch > ();
+    for (size_t i = 0; i < initial_size; ++i)
+    {
+        a.epoch->population.emplace_back(0, std::make_shared < TTTIndividual > (board_size));
+    }
+
+    size_t epoch = 0;
+    pEpoch new_epoch;
+    do
+    {
+        a.epoch->EpochBattle();
+        new_epoch = a.Selection(2.0 / a.epoch->population.size() * 100, 5, 70);
+
+        ++epoch;
+
+        std::cout << "======== Epoch " << epoch << " ========" << std::endl;
+
+        for each (auto &p in a.epoch->population)
+        {
+            std::cout << p.first << " ";
+        }
+        std::cout << std::endl;
+
+        a.epoch = new_epoch;
+
+        if (RandomBool(0.01))
+        {
+            a.epoch->population.emplace_back(0, std::make_shared < TTTIndividual > (board_size));
+        }
+    } while (epoch < max_epoch);
+
+    bool new_game = true;
+    while(new_game)
+    {
+        board b(board_size);
+        tic_tac winner;
+
+        print_ttt_board(b);
+
+        player_fn user_player = [] (board & _b, tic_tac _who)
+        {
+            size_t cell_i, cell_j;
+            cin >> cell_i >> cell_j;
+            _b.cells[(cell_i - 1) * _b.n + (cell_j - 1)] = ToFloat(_who);
+            print_ttt_board(_b);
+            return true;
+        };
+
+        TTTIndividual & other = * dynamic_cast < TTTIndividual * > (a.epoch->population.front().second.get());
+
+        player_fn other_player = [&other] (board & _b, tic_tac _who)
+        {
+            if (other.player(_b, _who))
+            {
+                print_ttt_board(_b);
+                return true;
+            }
+            return false;
+        };
+
+        winner = PlayTTT(b, tic_tac::X, user_player, other_player);
+
+        if (winner == tic_tac::X)
+        {
+            std::cout << "You win!" << std::endl;
+        }
+        else if (winner == tic_tac::O)
+        {
+            std::cout << "You loose!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Well, let's try again..." << std::endl;
+        }
+        std::cout << "[input] Try again? [1/0] ";
+        std::cin >> new_game;
+        std::cout << std::endl;
+    }
+}
+
 int main()
 {
 	cout << "Hello!" << endl;
